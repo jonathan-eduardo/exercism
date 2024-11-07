7b0f9c9cd6aba5cd4e7e70091594a811dd3fe521
Jonathan Silva <jonathanedsilva@gmail.com>
2024-11-07 16:45:34 -0300

    add city-office solution

---
+++ php/city-office/Address.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+class Address
+{
+    public string $street;
+    public string $postal_code;
+    public string $city;
+}
---
+++ php/city-office/Form.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+class Form
+{
+    public function blanks(int $length): string
+    {
+        return str_repeat(" ", $length);
+    }
+
+    public function letters(string $word): array
+    {
+        return mb_str_split($word);
+    }
+
+    public function checkLength(string $word, int $max_length): bool
+    {
+        $difference = mb_strlen($word) - $max_length;
+        return $difference <= 0;
+    }
+
+    public function formatAddress(Address $address): string
+    {
+        $formatted_street = mb_strtoupper($address->street);
+        $formatted_postal_code = mb_strtoupper($address->postal_code);
+        $formatted_city = mb_strtoupper($address->city);
+
+        return <<<FORMATTED_ADDRESS
+            $formatted_street
+            $formatted_postal_code $formatted_city
+            FORMATTED_ADDRESS;
+    }
+}
